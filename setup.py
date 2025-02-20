c5a661839e55abc8b79b046bba0f0e67a78e88ec
Jacopo Simoni <simonij@tcd.ie>
2025-02-20 11:55:57 -0600

    change license in setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
 	name='ExcPhCoupl',
 	version='0.0.1',
-	license='LICENSE',
+	license='gpl-3.0',
 	long_description='README.md',
 	packages=find_packages(),
 	install_requires=[
