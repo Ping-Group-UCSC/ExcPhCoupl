5a969615e0606f813e837598caed21daa0425ee0
Jacopo Simoni <simonij@tcd.ie>
2025-03-11 20:21:19 -0500

    read el-ph data

--- src/read_io.py
+++ src/read_io.py
@@ -5,6 +5,8 @@ from numpy import linalg as LA
 from netCDF4 import Dataset
 import itertools
 from logmod import log
+from common.param import p
+from common.constants import meV, eps_acustic
 
 # Importing specific functions and parameters from user-defined modules
 from common.param import *
@@ -21,7 +23,8 @@ def read_qpoints_yambo():
 
 def read_bse_wavefunction(Q_yambo, A_exc, exc_freq):
 	"""Read the finite Q BSE wavefunction from Yambo."""
-	for fragment_ind in range(N_Q):
+	log.debug("\t NQ: " + str(p.N_Q))
+	for fragment_ind in range(p.N_Q):
 		f = Dataset(path_ex + f'ndb.BS_diago_Q{fragment_ind+1}', 'r')
 		BS_EIGENSTATES = f.variables['BS_EIGENSTATES']
 		BS_Energies = f.variables['BS_Energies']	
@@ -35,21 +38,24 @@ def read_bse_wavefunction(Q_yambo, A_exc, exc_freq):
 	
 def read_elph_data(Q_yambo, g_elph, ph_freq):
 	"""Read the electron-phonon coupling data and phonon frequencies."""
-	for fragment_ind in range(N_Q):
-		f = Dataset(path_elph_data + str(fragment_ind + 1), 'r')
+	log.debug("\t elph data: " + p.path_elph_data)
+	for fragment_ind in range(p.N_Q):
+		f = Dataset(p.path_elph_data + str(fragment_ind + 1), 'r')
 		ELPH_fragment = f.variables[f'ELPH_GKKP_Q{fragment_ind+1}']
 		PH_FREQS_fragment = f.variables[f'PH_FREQS{fragment_ind+1}']
 		
-		arr_ELPH_fragment = np.array(ELPH_fragment)[:, N_e-N_v:N_e+N_c, N_e-N_v:N_e+N_c, :, :]
-		arr_PH_FREQS_fragment = np.array(PH_FREQS_fragment)[0:nmodes]
+		arr_ELPH_fragment = np.array(ELPH_fragment)[:, p.N_e-p.N_v:p.N_e+p.N_c, p.N_e-p.N_v:p.N_e+p.N_c, :, :]
+		arr_PH_FREQS_fragment = np.array(PH_FREQS_fragment)[0:p.nmodes]
 		
 		iQ = k2ik(Q_yambo[fragment_ind])
+		log.debug("\t iQ: " + str(iQ) + " -> " + str(Q_yambo[fragment_ind]))
 		arr_temp = arr_ELPH_fragment[:, :, :, :, 0] + 1j * arr_ELPH_fragment[:, :, :, :, 1]
-		g_temp = np.array(arr_temp).reshape((N_k, N_c + N_v, N_c + N_v, nmodes))
-		
+		g_temp = np.array(arr_temp).reshape((p.N_k, p.N_c+p.N_v, p.N_c+p.N_v, p.nmodes))
+
 		ph_freq[iQ] = arr_PH_FREQS_fragment
-		for k_fragment_ind in range(N_k):
+		for k_fragment_ind in range(p.N_k):
 			ik = k2ik(Q_yambo[k_fragment_ind])
+			log.debug("\t \t ik: " + str(ik) + " -> " + str(Q_yambo[k_fragment_ind]))
 			g_elph[iQ, ik] = g_temp[k_fragment_ind]
 			
 	return g_elph, ph_freq
@@ -57,11 +63,12 @@ def read_elph_data(Q_yambo, g_elph, ph_freq):
 def normalize_elph(g_elph, ph_freq):
 	"""Normalize the electron-phonon matrix with phonon frequencies."""
 	ph_freq = np.sqrt(np.abs(ph_freq))
-	print('Phonon frequencies @ Gamma (meV):', ph_freq[0] / meV)
-	for q_ind, l_ind in itertools.product(range(N_Q), range(nmodes)):
+	log.info('\t Phonon frequencies @ Gamma (meV):' + str(ph_freq[0]/meV))
+	for q_ind, l_ind in itertools.product(range(p.N_Q), range(nmodes)):
 		if np.abs(ph_freq[q_ind, l_ind]) > eps_acustic:
 			g_elph[q_ind, :, :, :, l_ind] /= np.sqrt(2. * ph_freq[q_ind, l_ind])
 		else:
+			log.warning("\t small phonon frequency: " + str(ph_freq[q_ind,l_ind]) + " < " + str(eps_acustic))
 			g_elph[q_ind, :, :, :, l_ind] = 0.
-	print('Electron-phonon matrix size:', g_elph.shape)
+	log.info('\t Electron-phonon matrix size: ' + str(g_elph.shape))
 	return g_elph, ph_freq
