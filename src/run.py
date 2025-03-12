7b4fd05f19c02fd00a5a0dd958a96abba30b4af9
Jacopo Simoni <simonij@tcd.ie>
2025-03-11 20:21:44 -0500

    create output dir

--- src/run.py
+++ src/run.py
@@ -2,6 +2,7 @@ from logmod import log
 from read_input_data import read_data
 from input_parser import parser
 from common.param import p
+import os
 
 def main():
 	yml_input = parser.parse_args().yml_inp[0]
@@ -9,6 +10,8 @@ def main():
 	log.debug("\t ++++++++++++++++++++++++++++++  START READING INPUT DATA ++++++++++++++++++++++++++++++ ")
 	p.read_input_parameters(yml_input)
 	log.debug("\t data dir " + p.data_dir)
+	if not os.path.exists(p.output_data_dir):
+		os.mkdir(p.output_data_dir)
 	read_data()
 	
 if __name__ == '__main__':
