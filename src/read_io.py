376adf5ef583aa7354a91a7f14f7a04fc431cce2
Jacopo Simoni <simonij@tcd.ie>
2025-05-30 13:39:15 -0500

    print data in MPI_ROOT only

--- src/read_io.py
+++ src/read_io.py
@@ -4,9 +4,11 @@ import numpy as np
 from numpy import linalg as LA
 from netCDF4 import Dataset
 import itertools
+import os
 from logmod import log
 from common.param import p
 from common.constants import meV, eps_acustic
+from mpi_module import mpi, MPI_ROOT
 
 # Importing specific functions and parameters from user-defined modules
 from common.param import *
@@ -15,15 +17,18 @@ from common.func import *
 def read_qpoints_yambo():
 	"""Read q-points from Yambo and convert them to the appropriate coordinate system."""
 	qpt_fil = p.data_dir + '/qpoints_yambo'
-	log.debug("\t qpt file: " + qpt_fil)
+	if mpi.rank == MPI_ROOT:
+		log.debug("\t qpt file: " + qpt_fil)
 	Q_yambo_rku = np.genfromtxt(qpt_fil, skip_header=1)[:, 0:3]
 	Q_yambo = np.einsum('ij,ni->nj', LA.inv(b_iku), Q_yambo_rku)
-	log.info("Q_yambo: " + str(Q_yambo))
+	if mpi.rank == MPI_ROOT:
+		log.info("Q_yambo: " + str(Q_yambo))
 	return Q_yambo
 
 def read_bse_wavefunction(Q_yambo, A_exc, exc_freq):
 	"""Read the finite Q BSE wavefunction from Yambo."""
-	log.debug("\t NQ: " + str(p.N_Q))
+	if mpi.rank == MPI_ROOT:
+		log.debug("\t NQ: " + str(p.N_Q))
 	for fragment_ind in range(p.N_Q):
 		f = Dataset(path_ex + f'ndb.BS_diago_Q{fragment_ind+1}', 'r')
 		BS_EIGENSTATES = f.variables['BS_EIGENSTATES']
@@ -38,7 +43,8 @@ def read_bse_wavefunction(Q_yambo, A_exc, exc_freq):
 	
 def read_elph_data(Q_yambo, g_elph, ph_freq):
 	"""Read the electron-phonon coupling data and phonon frequencies."""
-	log.debug("\t elph data: " + p.path_elph_data)
+	if mpi.rank == MPI_ROOT:
+		log.debug("\t elph data: " + p.path_elph_data)
 	for fragment_ind in range(p.N_Q):
 		f = Dataset(p.path_elph_data + str(fragment_ind + 1), 'r')
 		ELPH_fragment = f.variables[f'ELPH_GKKP_Q{fragment_ind+1}']
@@ -48,14 +54,16 @@ def read_elph_data(Q_yambo, g_elph, ph_freq):
 		arr_PH_FREQS_fragment = np.array(PH_FREQS_fragment)[0:p.nmodes]
 		
 		iQ = k2ik(Q_yambo[fragment_ind])
-		log.debug("\t iQ: " + str(iQ) + " -> " + str(Q_yambo[fragment_ind]))
+		if mpi.rank == MPI_ROOT:
+			log.debug("\t iQ: " + str(iQ) + " -> " + str(Q_yambo[fragment_ind]))
 		arr_temp = arr_ELPH_fragment[:, :, :, :, 0] + 1j * arr_ELPH_fragment[:, :, :, :, 1]
 		g_temp = np.array(arr_temp).reshape((p.N_k, p.N_c+p.N_v, p.N_c+p.N_v, p.nmodes))
 
 		ph_freq[iQ] = arr_PH_FREQS_fragment
 		for k_fragment_ind in range(p.N_k):
 			ik = k2ik(Q_yambo[k_fragment_ind])
-			log.debug("\t \t ik: " + str(ik) + " -> " + str(Q_yambo[k_fragment_ind]))
+			if mpi.rank == MPI_ROOT:
+				log.debug("\t \t ik: " + str(ik) + " -> " + str(Q_yambo[k_fragment_ind]))
 			g_elph[iQ, ik] = g_temp[k_fragment_ind]
 			
 	return g_elph, ph_freq
@@ -63,12 +71,19 @@ def read_elph_data(Q_yambo, g_elph, ph_freq):
 def normalize_elph(g_elph, ph_freq):
 	"""Normalize the electron-phonon matrix with phonon frequencies."""
 	ph_freq = np.sqrt(np.abs(ph_freq))
-	log.info('\t Phonon frequencies @ Gamma (meV):' + str(ph_freq[0]/meV))
+	if mpi.rank == MPI_ROOT:
+		log.info('\t Phonon frequencies @ Gamma (meV):' + str(ph_freq[0]/meV))
 	for q_ind, l_ind in itertools.product(range(p.N_Q), range(nmodes)):
 		if np.abs(ph_freq[q_ind, l_ind]) > eps_acustic:
 			g_elph[q_ind, :, :, :, l_ind] /= np.sqrt(2. * ph_freq[q_ind, l_ind])
 		else:
 			log.warning("\t small phonon frequency: " + str(ph_freq[q_ind,l_ind]) + " < " + str(eps_acustic))
 			g_elph[q_ind, :, :, :, l_ind] = 0.
-	log.info('\t Electron-phonon matrix size: ' + str(g_elph.shape))
+	if mpi.rank == MPI_ROOT:
+		log.info('\t Electron-phonon matrix size: ' + str(g_elph.shape))
 	return g_elph, ph_freq
+
+def save_Geph(G, iQ1, iQ2, excph_path):
+	"""Save computed results to files."""
+	for Q_ind in range(iQ1, iQ2):
+		G[Q_ind-iQ1].tofile(excph_path+f'/excph_Q{Q_ind + 1}.dat')
